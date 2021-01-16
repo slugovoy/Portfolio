26933d1ab5ba6acc572233785060a795e8c14b5e
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-15 19:12:17 -0700

    set up basic structure

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2020 Copyright:
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
