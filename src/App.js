5c9ee3d24cf8d8d2bc69ede9a4eef45399a7f784
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-15 20:50:28 -0700

    added api call to gitconnect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 function App() {
+
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    // NOTE: Use your username below
+    fetch('https://gitconnected.com/v1/portfolio/slugovoy')
+      .then(res => res.json())
+      .then(user => {
+        setUser(user);
+      });
+  }, []);
+
+  if (!user) {
+    return <div />;
+  }
+  console.log(user)
   return (
     <div className="App">
     <h1>Hello World!</h1>
